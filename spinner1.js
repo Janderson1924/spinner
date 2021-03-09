666eab45ac6aa01ca1b90c36ae275372b60a2478
Justin <justinanderson101@gmail.com>
2021-03-09 22:31:18 +0000

    spinner function

--- spinner1.js
+++ spinner1.js
@@ -11,7 +11,7 @@ setTimeout(() => {
 }, 500);
 
 setTimeout(() => {
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
 
 setTimeout(() => {
@@ -27,7 +27,7 @@ setTimeout(() => {
 }, 1300);
 
 setTimeout(() => {
-  process.stdout.write('\r\   ');
+  process.stdout.write('\r   ');
 }, 1500);
 
 setTimeout(() => {

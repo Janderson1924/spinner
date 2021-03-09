89f8e2886c32094a7f810f3c14627e44d767412c
Justin <justinanderson101@gmail.com>
2021-03-09 22:31:36 +0000

    refactored spinner function

--- spinner2.js
+++ spinner2.js
@@ -2,12 +2,8 @@ let delay = 0;
 const newChar = ['|', '/', '-', '\\', '|', '/', '-', '\\'];
 
 for (const char of newChar) {
-delay += 200;
-setTimeout(() => {
-  process.stdout.write(`\r${char}     `);
-}, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write(`\r${char}     `);
+  }, delay);
 }
-
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
